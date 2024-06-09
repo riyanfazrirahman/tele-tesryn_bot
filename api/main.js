84d63ea55b9173354f6c3ec711c423243dcfe8f2
riyanfazrirahman <riyanfazrirahman@gmail.com>
2024-06-10 00:36:16 +0700

    vercel deploy bot

--- api/main.js
+++ api/main.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const TelegramBot = require("node-telegram-bot-api");
+const fetch = require("node-fetch");
 
 // Replace 'YOUR_BOT_TOKEN' with your actual bot token
 const token = "6109169471:AAE9Xdyv7yu5tyXznq4AEq8ptEn5m5bIJ6E";
 
 // Create a new bot instance
-const tesbot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 const prefix = "/";
 const hiTes = new RegExp(`^${prefix}tes$`);
 const gempa = new RegExp(`^${prefix}gempa$`);
@@ -15,29 +16,29 @@ const app = express();
 app.use(bodyParser.json());
 
 // Set webhook route
-app.post(`/bot${token}`, (req, res) => {
+app.post(`/api/bot${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
 
 // Ketika bot dijalankan
-tesbot.onText(/^\/start$/, (msg) => {
+bot.onText(/^\/start$/, (msg) => {
   const commandsList = `
 Halo! Saya adalah bot gempa bumi.
 Berikut adalah daftar perintah yang dapat Anda gunakan:
-  
+ 
 1. /tes - Untuk melakukan tes.
 2. /gempa - Untuk mendapatkan informasi tentang gempa terkini.
   `;
 
-  tesbot.sendMessage(msg.chat.id, commandsList);
+  bot.sendMessage(msg.chat.id, commandsList);
 });
 
-tesbot.onText(hiTes, (callback) => {
-  tesbot.sendMessage(callback.from.id, "Ada yg bisa di banting? -_-");
+bot.onText(hiTes, (callback) => {
+  bot.sendMessage(callback.from.id, "Ada yg bisa di banting? -_-");
 });
 
-tesbot.onText(gempa, async (callback) => {
+bot.onText(gempa, async (callback) => {
   const BMKG_ENDPOINT = "https://data.bmkg.go.id/DataMKG/TEWS/";
   const apiCall = await fetch(BMKG_ENDPOINT + "autogempa.json");
   const {
@@ -65,19 +66,19 @@ Kedalaman: ${Kedalaman}
 Wilayah: ${Wilayah}. 
 Potensi: ${Potensi}
 Dirasakan: ${Dirasakan}
-  
+ 
 Sumber:
 [BMKG (Badan Meteorologi, Klimatologi, dan Geofisika)](https://data.bmkg.go.id/gempabumi/)
 `;
 
-  tesbot.sendPhoto(callback.from.id, imgBMKG, {
+  bot.sendPhoto(callback.from.id, imgBMKG, {
     caption: resultText,
   });
 });
 
 // Set webhook
 const domain = "https://your-vercel-domain.vercel.app"; // Ganti dengan domain Vercel Anda
-bot.setWebHook(`${domain}/bot${token}`);
+bot.setWebHook(`${domain}/api/bot${token}`);
 
 // Start Express server
 const port = process.env.PORT || 3000;
